e5e308edf23c4e1f9233be5c66ffdc4dd1c9b118
quangsiDev <tranquangsidev@gmail.com>
2023-12-18 20:34:35 +0700

    update

--- src/Todos/FormTodo.tsx
+++ src/Todos/FormTodo.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FormTodo() {
-  return <div>FormTodo</div>;
+  const [title, setTitle] = useState<string>("Làm dự án cuối khoá");
+  const handleChangeForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+  const handleSubmit = () => {
+    console.log("yes");
+  };
+  return (
+    <div className="container">
+      <input
+        onChange={handleChangeForm}
+        value={title}
+        type="text"
+        className="form-control"
+        placeholder="Title Todo"
+      />
+      <button onClick={handleSubmit} className="btn btn-danger">
+        Add
+      </button>
+    </div>
+  );
 }
--- src/Todos/Todos.tsx
+++ src/Todos/Todos.tsx
@@ -10,3 +10,4 @@ export default function Todos() {
     </div>
   );
 }
+// todo object
